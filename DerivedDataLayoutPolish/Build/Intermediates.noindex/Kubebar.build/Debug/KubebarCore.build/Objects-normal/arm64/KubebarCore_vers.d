8c97c2854760a7f55b9eea5974ede98b954e64ef
derek chen <1624861+dereknex@users.noreply.github.com>
2026-06-04 09:44:05 +0800

    chore: remove derived data

---
+++ DerivedDataLayoutPolish/Build/Intermediates.noindex/Kubebar.build/Debug/KubebarCore.build/Objects-normal/arm64/KubebarCore_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/derek/.codex/worktrees/2515/kubebar/DerivedDataLayoutPolish/Build/Products/Debug/KubebarCore.framework/Modules/module.modulemap \
+  /Users/derek/.codex/worktrees/2515/kubebar/DerivedDataLayoutPolish/Build/Intermediates.noindex/Kubebar.build/Debug/KubebarCore.build/DerivedSources/KubebarCore_vers.c
